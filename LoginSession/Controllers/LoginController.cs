09a55708966d6bde9949bb0deb375eeb7489b66b
Paurnima Salve <paurnimas@kpit.com>
2019-05-08 16:21:18 +0530

    Add project files.

---
+++ LoginSession/Controllers/LoginController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using LoginSession.Models;
+
+namespace LoginSession.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Authorize(User user)
+        {
+            using (LoginDetailEntities db = new LoginDetailEntities())
+            {
+                var userDetails = db.Users.Where(x => x.UserName == user.UserName && x.Password == user.Password).FirstOrDefault();
+                if (userDetails == null)
+                {
+                    user.LoginErrorMessage = "Invalid values added.";
+                    return View("Index", user);
+                }
+                else
+                {
+                    Session["UserID"] = user.UserID;
+                    Session["UserName"] = user.UserName;
+                    return RedirectToAction("Index", "Home");
+                }
+            }
+        }
+        public ActionResult Logout(User user)
+        {
+            Session.Abandon();
+            return RedirectToAction("Index", "Login");
+        }
+
+    }
+}
